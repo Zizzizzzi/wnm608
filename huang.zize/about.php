9fcbf18deb1c537aeb476c04265d11e74b441796
unknown <you@example.com>
2022-07-14 06:44:27 +0800

    module 7 project

---
+++ huang.zize/about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About Zizzi</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About Zizzi Art Store</h2>
+
+			<p>This is information about Zizzi # <?= $_GET['id'] ?>
+			</p>
+		</div>
+	</div>
+	
+</body>
+</html>
